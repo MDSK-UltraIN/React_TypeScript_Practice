27af9106beb72b06064f03b8be05e3efbdd38e38
TW-IT-Jouven Liao <jouven.liao@shoalter.com>
2023-09-12 14:06:26 +0800

    feat: 新增 New Todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./App.css";
 import Todos from "./components/Todos";
 import Todo from "./Models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ]
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodoList((pervTodoList) => {
+      return pervTodoList.concat(newTodo);
+    });
+    
+  }
 
   return (
     <div>
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todoList} />
     </div>
   );
 }
